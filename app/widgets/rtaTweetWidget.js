575f16139757886ce32f99d62f1835757204eac1
Dylan Yu <dylan.yu@mail.utoronto.ca>
2015-10-17 11:21:31 -0400

    Update rtaTweetWidget.js

--- app/widgets/rtaTweetWidget.js
+++ app/widgets/rtaTweetWidget.js
@@ -4,7 +4,7 @@ angular.module('app').directive('rtaTweetWidget', [
     function () {
         return {
             // inherited scope from psWidgetBody
-            templateUrl: '/app/widgets/rtaTweetTemplate.html',
+            templateUrl: 'app/widgets/rtaTweetTemplate.html',
             link: function (scope, el, attrs) {
 
                 scope.error = false;
